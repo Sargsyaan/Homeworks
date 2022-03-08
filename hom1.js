0c22e0666285dc35de0ef200831bacb998938721
Sargsyaan <100372493+Sargsyaan@users.noreply.github.com>
2022-03-08 22:43:33 +0300

    Add files via upload

---
+++ hom1.js
@@ -0,0 +1,91 @@
+ //HOMEWORK N3
+let n = prompt("Enter a number: ");
+if(n % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
+// HOMEWORK N4
+let n =prompt("Enter your number")
+if (n < 0){
+    console.log("yes")
+}else{
+    console.log("no")
+}
+//HOMEWORK N5
+let x = prompt("Enter your number")
+let y = prompt("Enter your number")
+if ( x / y && x < y){
+    console.log("1")
+}else {
+    console.log("0")
+}
+//HOMEWORK N6
+switch ("Jenuary"){
+    case 1:
+    break;
+}
+switch ("February"){
+    case 2:
+    break;
+}
+switch ("March"){
+    case 3:
+    break;
+}
+switch ("April"){
+    case 4:
+    break;
+}
+switch ("May"){
+    case 5:
+    break;
+}
+switch ("Jun"){
+    case 6:
+    break;
+}
+switch ("Jul"){
+    case 7:
+    break;
+}
+switch ("Ausgust"){
+    case 8:
+    break;
+}
+switch ("September"){
+    case 9:
+    break;
+}
+switch ("October"){
+    case 10:
+    break;
+}
+switch ("November"){
+    case 11:
+    break;
+}
+switch ("December"){
+    case 12:
+    break;
+}
+// Hetevyal coden chi ashxatum  ete minchev hingshabti chuxxeci uremn djvaracel em u chem haskacel)))
+ 
+//Homework N7
+ 
+let x = prompt("NUMBER I")
+let y = prompt("Numger II")
+let z = prompt("Number III")
+if ( x <y && y > z){
+    console.log(y)
+}else if(x > z && y < x){
+    console.log(x)
+} else if(z > x && z > y){
+    console.log(z)
+}
+// Isk hetevyal coden ashxatum e bayc masamb chkaroxaca sxals gtnel.
+
+//Homework N2
+console.log("Իմ անունը Արփի  է, ես գաղտնիք տարեկան եմ»")
+
