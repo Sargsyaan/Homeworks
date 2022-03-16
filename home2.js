86697c93240b988a35c0cfd3621d021844785183
Sargsyaan <100372493+Sargsyaan@users.noreply.github.com>
2022-03-16 23:22:56 +0300

    Add files via upload

---
+++ home2.js
@@ -0,0 +1,32 @@
+//Homework 1
+let number = +prompt();
+let mid=0;
+let sum=0;
+while(number>9){
+mid=number%10
+sum+=mid;
+number=(number-mid)/10;
+}
+sum+=number;
+console.log(sum);
+//Homework 2 
+let a = +prompt()
+let b = +prompt()
+let c = +prompt()
+if(a + b > c){
+    console.log("yes")
+}else{ if(c + b > a){
+    console.log("no")    
+}else{ if(a + c > b)
+    console.log("yes")
+
+    }
+} 
+//Homework 4
+let i = +prompt()
+let mid=0
+let rew=0
+let 
+for(mid!==0; rew=rew%10; rew=rew+mid%10){
+    console.log(mid=i/10)
+} //es coden chashxatox ankap code e))
